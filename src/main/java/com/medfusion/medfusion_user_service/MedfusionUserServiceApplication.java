09947e1bc17e0ad13d40e8ee2e89d3c510762dbe
adarshrishu <adarshrishu08@gmail.com>
2025-02-27 15:39:41 +0530

    Medfusion User Service First Commit.

--- src/main/java/com/medfusion/medfusion_user_service/MedfusionUserServiceApplication.java
+++ src/main/java/com/medfusion/medfusion_user_service/MedfusionUserServiceApplication.java
@@ -1,5 +1,8 @@
 package com.medfusion.medfusion_user_service;
 
+import com.netflix.discovery.EurekaClient;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,8 +11,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class MedfusionUserServiceApplication {
 
+	@Autowired
+	private EurekaClient eurekaClient;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MedfusionUserServiceApplication.class, args);
 	}
 
+	@PreDestroy
+	public void deregisterFromEureka() {
+		System.out.println("Deregistering from Eureka...");
+		eurekaClient.shutdown();
+	}
 }
